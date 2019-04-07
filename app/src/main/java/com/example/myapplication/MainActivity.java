b0090af813697dcb0abfe5e517ce9b9dfdd6d54b
fjnu-zexin <123517323@qq.com>
2019-04-07 13:22:39 +0800

    test2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,37 +9,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i("MainActivityLife","调用onCreate()");
+        setContentView(R.layout.layout2);
+
     }
     @Override
     protected void onStart(){
         super.onStart();
-        Log.i("MainActivityLife","调用onStart()");
+
     }
     @Override
     protected void onResume(){
         super.onResume();
-        Log.i("MainActivityLife","调用onResume()");
+
     }
     @Override
     protected void onPause(){
         super.onPause();
-        Log.i("MainActivityLife","调用onPause()");
+
     }
     @Override
     protected void onStop(){
         super.onStop();
-        Log.i("MainActivityLife","调用onStop()");
+
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.i("MainActivityLife","调用onDestroy()");
+
     }
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.i("MainActivityLife","调用onRestart()");
+
     }
 }
